8d0607caefc434ebcecde32d8b007edaa2344feb
Carlos C <elcordata@gmail.com>
2025-04-24 17:50:25 -0400

    Added tests for class attribute

---
+++ ui-tests/class-attribute.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from '@playwright/test';
+
+test.describe.only('Class Attribute Tests', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('http://uitestingplayground.com/classattr'); 
+    });
+
+    test('Clicking on green button', async ({ page }) => {
+        // No action gets triggered when clicking on the green button
+       await page.click('.btn.btn-success'); 
+    });
+
+    test('Clicking on yellow button', async ({ page }) => {
+        // No action gets triggered when clicking on the yellow button
+       await page.click('.btn.btn-warning'); 
+    });
+
+    test('Clicking on blue button using and handling pop-up alert', async ({ page }) => {
+        // This one should trigger a pop-up alert
+        // and we will handle it using the page.on('dialog') event
+        await page.click('.btn.btn-primary');
+        page.on('dialog', async dialog => {
+            expect(dialog.message()).toBe('Primary button pressed');
+            await dialog.accept();
+        });
+    });
+});
